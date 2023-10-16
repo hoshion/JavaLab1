34306cee08c6196cb33b3c15c5e8cde805909e17
just-hoshinon <mwadman2018@gmail.com>
2023-10-17 00:21:17 +0300

    created first lab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Lab lab = new Lab();
 
-        double res = lab.calc(1, 100, 1, 20);
+        double res = lab.calc(Long.parseLong(args[0]), Long.parseLong(args[1]), Long.parseLong(args[2]), Long.parseLong(args[3]));
 
         System.out.println("Result of operation is " + res);
     }
